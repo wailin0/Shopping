089aaef6e4a18640bbe0eb7c0831b1230830c775
Wai Lin <leon.wai@live.com>
2020-03-26 02:53:14 +0630

    Finished angularjs implementation for shop ui page

--- src/main/resources/static/js/datatable.js
+++ src/main/resources/static/js/datatable.js
@@ -1,21 +1,32 @@
-
+//I used pagination plugin
       var app = angular.module('ShopApp', ['angularUtils.directives.dirPagination']);
       
       app.controller('ProductsController', function($scope, $http) {
-    	  $scope.currentPage = 1;
-    	  $scope.value="";
-          $http.get('/rest/products/'+$scope.value).
+    	  $scope.currentPage = 1;     //current page for paging plugin
+    	  $scope.selectCategory="";
+    	  
+    	  //for showing all products
+    	  $http.get('/rest/products/').
+          then(function(response) {
+              $scope.products = response.data;
+          });
+    	  
+    	  //for showing selected products by category
+    	  $scope.select = function(){
+          $http.get('/rest/products/'+$scope.selectCategory).
               then(function(response) {
                   $scope.products = response.data;
               });
+    	  };
       });
       
 
+      //for pagination plugin
       function PagingController($scope) {
         $scope.pageChangeHandler = function(num) {
         	
         };
       }
-
+      
       app.controller('PagingController', PagingController);
       
